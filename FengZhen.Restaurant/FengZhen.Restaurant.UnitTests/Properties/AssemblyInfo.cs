f362fcfc462aaafd97797df968b5beea099584e6
fengzhen1980 <tj.fengzhen@hotmail.com>
2020-03-09 00:19:51 -0400

    add unitTests project

---
+++ FengZhen.Restaurant/FengZhen.Restaurant.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FengZhen.Restaurant.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("FengZhen.Restaurant.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dbfb2b50-ffe4-4462-9489-40eba1e5c8df")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
