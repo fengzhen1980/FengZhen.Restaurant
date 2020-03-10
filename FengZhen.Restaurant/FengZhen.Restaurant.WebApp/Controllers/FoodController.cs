d4c06881b6782fed338be4d85e906aae5a6ba3c0
fengzhen1980 <tj.fengzhen@hotmail.com>
2020-03-09 21:45:41 -0400

    add FoodController

---
+++ FengZhen.Restaurant/FengZhen.Restaurant.WebApp/Controllers/FoodController.cs
@@ -0,0 +1,25 @@
+﻿using FengZhen.Restaurant.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FengZhen.Restaurant.WebApp.Controllers
+{
+    public class FoodController : Controller
+    {
+        public IFoodsRepository FoodsRepository { get; set; }
+
+        //// GET: Food
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ViewResult List()
+        {
+            return View(FoodsRepository.FoodsList);
+        }
+    }
+}
